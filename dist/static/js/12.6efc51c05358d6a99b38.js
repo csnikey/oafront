792eb7e53e8c602a88b5ae0553ba23d7e7ffd98c
csnikey <1484054651@qq.com>
2017-09-12 14:28:29 +0800

    提交

---
+++ dist/static/js/12.6efc51c05358d6a99b38.js
@@ -0,0 +1,1 @@
+webpackJsonp([12],{234:function(e,n,t){var r=t(54)(t(394),t(494),null,null,null);e.exports=r.exports},394:function(e,n,t){"use strict";Object.defineProperty(n,"__esModule",{value:!0}),n.default={data:function(){return{}}}},494:function(e,n){e.exports={render:function(){var e=this,n=e.$createElement;return(e._self._c||n)("div",[e._v("学校档案管理")])},staticRenderFns:[]}}});
